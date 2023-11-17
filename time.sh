04083832d3131335f9d8de57804d021c82c65e5e
Allen Jue <mrallenjue@yahoo.com>
2023-11-16 21:28:45 -0600

    Trying script

--- time.sh
+++ time.sh
@@ -1,1 +1,24 @@
-sudo time ./threadpool -n 0 -m 0 -i ~/Downloads/Cs378-Concurrency/threadpool/top_websites.txt > ~/Downloads/Cs378-Concurrency/threadpool/results/my_16.txt
+# Name of the text file to save the results
+input_file="/Users/allenjue/Downloads/Cs378-Concurrency/threadpool/top_websites.txt"
+
+time_file="/Users/allenjue/Downloads/Cs378-Concurrency/threadpool/seq_timing.csv"
+
+fail_file="/Users/allenjue/Downloads/Cs378-Concurrency/threadpool/seq_fail.csv"
+# Clear the file if it already exists
+> $time_file
+> $fail_file
+
+echo "threads, execTime" > $time_file
+echo "threads, totalFails" > $fail_file
+
+mode=1
+num_threads=(1 2 4 8 16 32 64 128 256 512 1024)
+
+for threads in "${num_threads[@]}"; do
+  start_time=$(date +%s.%N)  
+  ./threadpool -n $threads -m $mode -i $input_file
+  end_time=$(date +%s.%N) 
+
+  cur_time=$(echo "$end_time - $start_time" | bc -l)
+  echo "$threads, $cur_time" >> $time_file
+done
