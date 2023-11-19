da75e506bd80dae9aa25c1e06179659924467a49
Allen Jue <mrallenjue@yahoo.com>
2023-11-19 00:29:28 -0600

    Update c_timing

--- dataView.R
+++ dataView.R
@@ -1,8 +1,8 @@
 library(dplyr)
 library("viridis")         
 
-avg.seq.time = 9.671273700
-avg.seq.fail = 5 / (42 * 10)
+avg.seq.time = 4.025366900
+avg.seq.fail = 0
 avg.seq.fail
 
 df.c.time = read.csv("./Downloads/Cs378-Concurrency/threadpool/timing/c_timing.csv")
@@ -35,8 +35,10 @@ plotGraph2 <- function(results, results2, title, legend, colors) {
     col = colors[2],
     lwd = 3
   )
-  legend("topleft", legend = legend, 
+  legend("topright", legend = legend, 
          fill = colors, cex = 0.8)
+  # Add a red horizontal line at y = 1
+  abline(h = 1, col = "red", lty = 2)
 }
 
 plotGraph <- function(results, title, legend, colors) {
@@ -50,8 +52,48 @@ plotGraph <- function(results, title, legend, colors) {
        col = colors[1],
        pch = 19,
        lwd = 3)
+  legend("topright", legend = legend, 
+         fill = colors, cex = 0.8)
+}
+
+### Methods to handle graphing fails
+plotGraphFail <- function(results, title, legend, colors) {
+  plot(log(results$threads),
+       results$totalFails, 
+       type = "o",
+       main = title,
+       xlab = "Log(Number of Threads in Threadpool)",
+       ylim=range(0, max(results$totalFails) * 1.1),
+       ylab = "Fail Increase",
+       col = colors[1],
+       pch = 19,
+       lwd = 3)
   legend("topleft", legend = legend, 
          fill = colors, cex = 0.8)
+}
+
+plotGraphFails2 <- function(results, results2, title, legend, colors) {
+  plot(log(results$threads),
+       results$totalFails, 
+       type = "o",
+       main = title,
+       xlab = "Log(Number of Threads in Threadpool)",
+       ylim=range(0, max(max(results$totalFails), max(results2$totalFails)) * 1.1),
+       ylab = "Fail Increase",
+       col = colors[1],
+       pch = 19,
+       lwd = 3)
+  lines(
+    log(results2$threads),
+    results2$totalFails,
+    type = "o",
+    col = colors[2],
+    lwd = 3
+  )
+  legend("topleft", legend = legend, 
+         fill = colors, cex = 0.8)
+  # Add a red horizontal line at y = 1
+  abline(h = 1, col = "red", lty = 2)
 }
 
 palette_colors <- viridis(8)
@@ -72,11 +114,21 @@ df.my.fails = read.csv("./Downloads/Cs378-Concurrency/threadpool/timing/my_fail.
 
 # calculate the change in avg fail rate for the threadpools
 df.c.total.fails <- aggregate(totalFails ~ threads, data = df.c.fails, sum)
-df.c.total.fails$totalFails = avg_seq_fail / (df.c.total.fails$totalFails / (42 * 10)) 
+df.c.total.fails$totalFails = (df.c.total.fails$totalFails / (42 * 10)) / avg_seq_fail 
 df.c.total.fails
 
 df.my.total.fails <- aggregate(totalFails ~ threads, data = df.my.fails, sum)
-df.my.total.fails$totalFails = avg_seq_fail / (df.my.total.fails$totalFails / (42 * 10))
+df.my.total.fails$totalFails =  (df.my.total.fails$totalFails / (42 * 10)) / avg_seq_fail 
 df.my.total.fails
 
+plotGraphFail(df.c.total.fails, "Boost ASIO Threadpool Fail Increase", 
+          c("Boost ASIO"), c(palette_colors[1]))
 
+plotGraphFail(df.my.total.fails, "Personal Threadpool Implementation Fail Increase", 
+          c("Personal Threadpool"), c(palette_colors[6]))
+
+plotGraphFails2(df.c.total.fails, 
+           df.my.total.fails, 
+           "Boost ASIO and Personal Implementation Fails Comparison",
+           c("Boost ASIO", "Personal Threadpool"),
+           c(palette_colors[1], palette_colors[6]))
